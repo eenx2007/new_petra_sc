686f474fd0e1a6d29f8da2b59b70cc90944444f7
Hendriansah <hendriansah@live.com>
2013-07-30 04:56:24 +0700

    wallpaper changer

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -177,7 +177,7 @@
 			if(detik<10)
 				detik="0"+detik;
 			$('#jam_utama').html(jam+':'+menit+':'+detik);
-			if(detik%5==0)
+			if(menit%10==0)
 			{
 				$('#cover-background').hide();
 				var bgimage = new Image();      
