42309a9a0e1e8d77633d33ad991c46732bf96ac9
Hendriansah <hendriansah@live.com>
2013-07-31 02:50:25 +0700

    finance report

--- application/views/admin_include/transaction_list.php
+++ application/views/admin_include/transaction_list.php
@@ -9,7 +9,8 @@
 </script>
         	<table class="main_table">
             	<tr><th>No.</th><th>Date</th><th>Transaction</th><th>Debit</th><th>Credit</th><th>User</th><th>Reff</th></tr>
-                <?php $i=0;$total_debit=0;$total_credit=0; foreach($query as $rows): $i++; ?>
+                <?php $i=0;$total_debit=0;$total_credit=0; foreach($query as $rows): ?>
+                	<?php if($rows->transaction_title<>"Income from Invoice I"):  $i++; ?>
                 	<tr>
                     	<td><?php echo $i;?></td>
                         <td><?php echo mdate('%d/%m/%Y %h:%i:%s',$rows->transaction_date);?></td>
@@ -24,6 +25,7 @@
                         <td><?php echo $rows->sure_name;?></td>
                         <td><?php echo $rows->transaction_reff;?></td>
                     </tr>
+                    <?php endif;?>
                 <?php endforeach;?>
                 <tr><td colspan="3">Total</td><td><?php echo number_format($total_debit,0,',','.');?></td><td><?php echo number_format($total_credit,0,',','.');?></td><td></td><td></td></tr>
                 <tr><td colspan="7"><button id="export_to_excel">Export to Excel</button></td></tr>
--- application/views/admin_include/transaction_list_etx.php
+++ application/views/admin_include/transaction_list_etx.php
@@ -1,6 +1,7 @@
 <table class="main_table">
             	<tr><th>No.</th><th>Date</th><th>Transaction</th><th>Debit</th><th>Credit</th><th>User</th><th>Reff</th></tr>
-                <?php $i=0;$total_debit=0;$total_credit=0; foreach($query as $rows): $i++; ?>
+                <?php $i=0;$total_debit=0;$total_credit=0; foreach($query as $rows): ?>
+                	<?php if($rows->transaction_title<>"Income from Invoice I"): $i++; ?>
                 	<tr>
                     	<td><?php echo $i;?></td>
                         <td><?php echo mdate('%d/%m/%Y %h:%i:%s',$rows->transaction_date);?></td>
@@ -13,8 +14,9 @@
                             <td><?php echo number_format($rows->transaction_total,0,',','.');$total_credit=$total_credit+$rows->transaction_total;?></td>
 						<?php endif;?>
                         <td><?php echo $rows->sure_name;?></td>
-                        <td><?php echo $rows->transaction_reff;?></td>
+                        <td><?php echo print_r($rows->transaction_reff,true);?></td>
                     </tr>
+                    <?php endif;?>
                 <?php endforeach;?>
                 <tr><td colspan="3">Total</td><td><?php echo number_format($total_debit,0,',','.');?></td><td><?php echo number_format($total_credit,0,',','.');?></td><td></td><td></td></tr>
                 
